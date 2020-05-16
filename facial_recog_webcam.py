a7327ee9ee9748087ac1fcea062c66931bc47e87
Nathan <ngoutcher1999@gmail.com>
2020-05-16 16:39:31 +0100

    renamed image facial recog app file and new webcam file

---
+++ facial_recog_webcam.py
@@ -0,0 +1,24 @@
+import cv2
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+videoCap = cv2.VideoCapture(0)
+
+def main():
+    while True:
+        ret, frame = videoCap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.35, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) and 0xFF == ord('q'):
+            break
+
+    videoCap.release()
+    cv2.destroyAllWindows()
